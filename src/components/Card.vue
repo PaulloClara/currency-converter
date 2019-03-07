be60004f928902f8a82c4cc78aeb6d07a643d657
Paulo Ricardo <paulloclara@gmail.com>
2019-03-07 20:56:59 -0300

    Criação do card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,18 @@
 <template>
 <div class="card">
-  {{ coin }}
+  <div class="card-header">
+    <h1 class="card-header-title is-centered">{{ coin.name }}</h1>
+  </div>
+  <div class="card-content">
+    <p>{{ coin.code }} {{ (inputBRL / coin.value).toFixed(2) }}</p>
+    <input class="input is-rounded" type="number" placeholder="BRL" v-model="inputBRL">
+    <br><br><br>
+    <p>BRL {{ (coin.value * inputCoin).toFixed(2) }}</p>
+    <input class="input is-rounded" type="number" :placeholder="coin.code" v-model="inputCoin">
+  </div>
+  <div class="card-footer">
+    <p class="card-footer-item">{{ coin.date }}</p>
+  </div>
 </div>
 </template>
 
@@ -10,6 +22,12 @@ export default {
   name: 'Card',
   props: {
     coin: {}
-  }
+  },
+  data(){
+    return{
+      inputCoin: '',
+      inputBRL: '',
+    }
+  },
 }
 </script>
