dc039054c57d4eaaead47a8c6d310aeddaabb48d
paulloclara <paulloclara@gmail.com>
2020-05-08 17:43:10 -0300

    feat: configure initial pwa

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,19 @@ const { NODE_ENV } = process.env;
 
 module.exports = {
   publicPath: NODE_ENV === "production" ? "/conversor-de-moedas/" : "/",
-  lintOnSave: false
+  lintOnSave: false,
+  pwa: {
+    name: "Conversor De Moedas",
+    display: "standalone",
+    themeColor: "#f7931a",
+    msTileColor: "#f7931a",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#f7931a",
+    manifestOptions: {
+      start_url: ".",
+      short_name: "ConversorM",
+      description: "Conversor de Moedas.",
+      background_color: "#ffdb19"
+    }
+  }
 };
